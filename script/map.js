0a2312e7fcb6a07c79a9a2d12e00d87fb886a2d6
KrisWSO <kris.williams@wsoglobal.com>
2021-08-05 17:19:13 -0400

    tweaks

--- script/map.js
+++ script/map.js
@@ -1,7 +1,7 @@
 // adding circle markers
 
 
-var mymap = L.map('mapid').setView([44.16235545311861, -72.85046914555758], 12);
+var mymap = L.map('mapid').setView([44.16235545311861, -72.85046914555758], 11);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
